afb7730d252468ac26bdd8916490a554364c71bc
schexi <victoria.skola@gmail.com>
2026-03-27 16:56:24 +0100

    Basfiler för Loans List React applikationen

---
+++ src/components/LoanService.jsx
@@ -0,0 +1,4 @@
+
+// Konstanter för att sätta fasta permanenta värden för APIets azure-url samt dess API-nyckel
+const BASE_URL = "loans-api-bye8b4fgaufucma9.norwayeast-01.azurewebsites.net";
+const API_KEY = "key1234";
